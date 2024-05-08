ae15347124c3e4d59447b5c1518454a7241cf561
Solomon <solomonuche@gmail.com>
2024-05-08 09:37:59 +0100

    LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+BasicCache module
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    LRU caching system
+    """
+
+    def __init__(self):
+        """initilizer"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        put items into cache
+        """
+
+        if key and item:
+            if len(self.cache_data.keys()) == BaseCaching.MAX_ITEMS:
+                key_to_remove = self.queue.pop(0)
+                print(f'DISCARD: {key_to_remove}')
+                del self.cache_data[key_to_remove]
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        access cache item
+        """
+        if key and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
