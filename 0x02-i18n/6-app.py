dad3604cb8fa4a6d371dd507b076fafaac670f95
Solomon <solomonuche@gmail.com>
2024-05-16 03:51:31 +0100

    Use user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -55,6 +55,9 @@ def get_locale():
         return locale
     if g.user and g.user['locale'] in app.config['LANGUAGES']:
         return g.user['locale']
+    header_locale = request.headers.get('locale', '')
+    if header_locale in app.config['LANGUAGES']:
+        return header_locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
